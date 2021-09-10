7d49db93faf3ebed436fd45d6489b363e3d40012
levan buxnikashvili <levanifenix.1234@gmail.com>
2021-09-10 19:00:06 +0400

    added upvotebutton

---
+++ resources/js/components/UpvoteButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button class="btn" @click="upvote"> <i class="fas fa-arrow-up"></i></button>
+  </div>
+</template>
+
+<script>
+export default {
+    props:[
+      'postId',
+      'isUpvoted',
+    ],
+    methods: {
+        upvote(){
+            axios.post("/upvote/" + this.postId).then(res=>{
+              console.log(res.data);
+            });
+            //console.log(this.postId);
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
